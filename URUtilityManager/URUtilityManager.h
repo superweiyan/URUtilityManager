dbf076e1b2052c0ef6ba8cb8a80451582ae4ad11
superweiyan <superweiyan@163.com>
2017-08-03 10:51:51 +0800

    Initial Commit

---
+++ URUtilityManager/URUtilityManager.h
@@ -0,0 +1,19 @@
+//
+//  URUtilityManager.h
+//  URUtilityManager
+//
+//  Created by weiyan on 03/08/2017.
+//  Copyright © 2017 weiyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for URUtilityManager.
+FOUNDATION_EXPORT double URUtilityManagerVersionNumber;
+
+//! Project version string for URUtilityManager.
+FOUNDATION_EXPORT const unsigned char URUtilityManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <URUtilityManager/PublicHeader.h>
+
+
